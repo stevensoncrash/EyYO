9bd7c0fb80928cbc927949c92fc6d9f4d0a753bc
Stevensoncrash <sjreyes1@yahoo.com>
2018-05-08 16:26:40 -0700

    SWReveal functions add to front/rear view controllers. Minor color matches from mockfiles.

---
+++ EyYo/Controllers/ChannelVC.swift
@@ -0,0 +1,20 @@
+//
+//  ChannelVC.swift
+//  EyYo
+//
+//  Created by Stephen Reyes on 5/8/18.
+//  Copyright © 2018 Stephen Reyes. All rights reserved.
+//
+
+import UIKit
+
+class ChannelVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.revealViewController().rearViewRevealWidth = self.view.frame.size.width - 60 
+
+    
+    }
+}
---
+++ EyYo/Controllers/ChatVC.swift
@@ -0,0 +1,29 @@
+//
+//  ChatVC.swift
+//  EyYo
+//
+//  Created by Stephen Reyes on 5/8/18.
+//  Copyright © 2018 Stephen Reyes. All rights reserved.
+//
+
+import UIKit
+
+class ChatVC: UIViewController {
+    
+    // Outlets
+    
+    @IBOutlet weak var menuBtn: UIButton!
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+ 
+        menuBtn.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
+        self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        self.view.addGestureRecognizer(self.revealViewController().tapGestureRecognizer())
+    }
+
+
+
+}
