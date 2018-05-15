65501b41de01517ab09875c9ca8cd98a7a254ac6
Stevensoncrash <sjreyes1@yahoo.com>
2018-05-14 19:12:46 -0700

    LoginVC UI completed

---
+++ EyYo/Controllers/LoginVC.swift
@@ -0,0 +1,23 @@
+//
+//  LoginVC.swift
+//  EyYo
+//
+//  Created by Stephen Reyes on 5/14/18.
+//  Copyright © 2018 Stephen Reyes. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+    
+    
+    @IBAction func closedBtnPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
---
+++ EyYo/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  EyYo
+//
+//  Created by Stephen Reyes on 5/10/18.
+//  Copyright © 2018 Stephen Reyes. All rights reserved.
+//
+
+import Foundation
+
+
+let TO_LOGIN = "toLogin"
